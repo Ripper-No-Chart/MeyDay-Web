cac31584aa1a389297738209b3e976fd321f413d
RipperNoChart <rippernochart@gmail.com>
2019-10-31 11:38:40 -0300

    menu corregido

--- interact/includes/header.php
+++ interact/includes/header.php
@@ -7,21 +7,23 @@
         <li class="nav-item active">
           <a href="new_entry.php" class="navbar-brand"><i class="fas fa-plus-square"></i> Nuevo registro</a>
         </li>
-        <div class="btn-group">
-          <a class="navbar-brand dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-search"></i> Busqueda</a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item lead" href="search.php"><i class="fas fa-search-plus"></i> Busqueda individual</a>
-            <a class="dropdown-item lead" href="listing.php"><i class="fas fa-file-invoice"></i> Busqueda general</a>
-          </div>
-        </div>
-        <div class="btn-group">
+        <div class="dropdown">
           <a class="navbar-brand dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-dollar-sign"></i> Liquidaciones</a>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          </a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
             <a class="dropdown-item lead" href="settlements.php"><i class="fas fa-hand-holding-usd"></i> Nueva liquidacion</a>
             <a class="dropdown-item lead" href="summary.php"><i class="far fa-chart-bar"></i> Resumen</a>
             <a class="dropdown-item lead" href="pendings.php"><i class="fas fa-receipt"></i> Proximas liquidaciones</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <a class="navbar-brand dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-search"></i> Busqueda</a>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item lead" href="search.php"><i class="fas fa-search-plus"></i> Busqueda individual</a>
+            <a class="dropdown-item lead" href="listing.php"><i class="fas fa-file-invoice"></i> Busqueda general</a>
           </div>
         </div>
       </ul>
